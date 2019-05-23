3c092d72227cdbc75c54c3123e5275e79f087545
vnarasa <v.narasa@gmail.com>
2019-05-23 14:47:49 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace AzureDeploySample
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString);
+            //SqlDataAdapter da = new SqlDataAdapter("select * from TestConnection", conn);
+            //System.Data.DataTable dt = new System.Data.DataTable();
+            //da.Fill(dt);
+
+            //gvData.DataSource = dt;
+            //gvData.DataBind();
+        }
+    }
+}
